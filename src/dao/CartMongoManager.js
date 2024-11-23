2675137a3e2c9bc0abc724f8494f3e422856626f
LeandroGastonLuna <lea21luna@gmail.com>
2024-11-23 13:11:48 -0300

    api/carts endpoints agregados

--- src/dao/CartMongoManager.js
+++ src/dao/CartMongoManager.js
@@ -1,12 +1,12 @@
 import { carritosModelo } from "./models/carritoModelo.js"
 
-export default class CartManager{
-    static async getCarts(){
+export default class CartManager {
+    static async getCarts() {
         return await carritosModelo.find().lean()
     }
 
-    static async getCartById(cartID){
-        let cart = await carritosModelo.findOne( {id:cartID} )
+    static async getCartById(cartID) {
+        let cart = await carritosModelo.findOne({ id: cartID })
         if (!cart) {
             return
         }
@@ -14,39 +14,66 @@ export default class CartManager{
     }
 
     static async createCart() {
-        let carts = await carritosModelo.find().lean()
+        let carts = await this.getCarts()
         let id = 1
-        if(carts.length>0){
-            id=Math.max(...carts.map(cart=>cart.id))+1
+        if (carts.length > 0) {
+            id = Math.max(...carts.map(cart => cart.id)) + 1
         }
         let orders = []
-        let cart = {id:id,products:orders}
+        let cart = { id: id, products: orders }
         let newCart = await carritosModelo.create(cart)
 
         return newCart.toJSON()
     }
 
-    static async addOrderToCart(cartID, productID){
+    static async addOrderToCart(cartID, productID) {
         let cart = await this.getCartById(cartID)
         let quantity = 1
-        
+
         let prodInCart = cart.products.find(prod => prod.product === productID)
-        if (prodInCart){
+        if (prodInCart) {
             cart.products.forEach(prod => {
-                if (prod.product === productID){
-                    ++prod.quantity 
-                }             
+                if (prod.product === productID) {
+                    ++prod.quantity
+                }
             });
         } else {
             cart.products.push(
                 {
-                    product:productID,
-                    quantity:quantity
+                    product: productID,
+                    quantity: quantity
                 }
             )
         }
-        console.log(cart)
-        return await carritosModelo.findOneAndUpdate({ id:cartID }, cart, { new: true })
+        return await carritosModelo.findOneAndUpdate({ id: cartID }, cart, { new: true })
+    }
+
+    static async updateCartProduct(cartID,productID,quantity) {
+        let cart = await this.getCartById(cartID)
+        let prodIndex = cart.products.findIndex(p => p.product === productID)
+
+        cart.products[prodIndex].quantity = quantity
+                                                        // $set -> operador para  reemplazar el valor de un campo con el valor especificado 
+        await carritosModelo.findOneAndUpdate({ id: cartID }, { $set: { products: cart.products } }, { new: true })
+    }
+
+    static async emptyCart(cartID) {
+        let cart = this.getCartById(cartID)
+        cart.products = []
+        await carritosModelo.updateOne({ id: cartID }, cart, { new: true })
+        return cart
+    }
+
+    static async removeProduct(cartID, productID) {
+        let cart = await this.getCartById(cartID)
+
+        let prodIndex = cart.products.findIndex(p => p.product === productID)
+        if (prodIndex === -1) {
+            return 
+        }
+        cart.products.splice(prodIndex, 1)
+                                                        // $set -> operador para  reemplazar el valor de un campo con el valor especificado 
+        await carritosModelo.findOneAndUpdate({ id: cartID }, { $set: { products: cart.products } }, { new: true })
     }
 }
 
--- src/routes/cartsMongoRouter.js
+++ src/routes/cartsMongoRouter.js
@@ -1,5 +1,5 @@
 import path from "path";
-import { Router } from 'express'; 
+import { Router } from 'express';
 import __dirname from "../utils.js";
 import CartManager from "../dao/CartMongoManager.js"
 import ProductManager from "../dao/ProductMongoManager.js"
@@ -8,9 +8,9 @@ import { pidCheck } from '../middlewares/idCheck.js';
 import { cidCheck } from '../middlewares/idCheck.js';
 import { isValidObjectId } from 'mongoose';
 
-export const router=Router()
+export const router = Router()
 
-router.get("/", async (req, res)=>{ 
+router.get("/", async (req, res) => {
     try {
         let cart = await CartManager.getCarts()
         res.status(200).send(cart)
@@ -19,24 +19,24 @@ router.get("/", async (req, res)=>{
     }
 })
 
-router.post("/", async (req, res)=>{
+router.post("/", async (req, res) => {
     try {
         await CartManager.createCart()
-        return res.status(200).send({CONFIRMATION:"CART CREATED."})
+        return res.status(200).send({ CONFIRMATION: "CART CREATED." })
     } catch (err) {
         return res.status(500).send({ ERROR: `${err.message}` })
     }
 })
 
-router.get("/:cid", cidCheck, async (req, res)=>{
+router.get("/:cid", cidCheck, async (req, res) => {
     if (!isValidObjectId(req.cid)) {
         res.setHeader('Content-Type', 'application/json');
         return res.status(400).json({ error: `Invalid Cart ID!` })
     }
     try {
         let cart1 = await CartManager.getCartById(req.cid)
-        if (!cart1){
-            return res.status(404).send({ERROR:`cart of id ${req.cid} NOT FOUND.`})
+        if (!cart1) {
+            return res.status(404).send({ ERROR: `cart of id ${req.cid} NOT FOUND.` })
         }
         return res.status(200).send(cart1)
     } catch (err) {
@@ -44,7 +44,7 @@ router.get("/:cid", cidCheck, async (req, res)=>{
     }
 })
 
-router.post("/:cid/product/:pid", cidCheck,pidCheck, async (req, res)=>{
+router.post("/:cid/product/:pid", cidCheck, pidCheck, async (req, res) => {
     if (!isValidObjectId(req.cid)) {
         res.setHeader('Content-Type', 'application/json');
         return res.status(400).json({ error: `Invalid Cart ID!` })
@@ -55,18 +55,105 @@ router.post("/:cid/product/:pid", cidCheck,pidCheck, async (req, res)=>{
     }
     try {
         let cart = await CartManager.getCartById(req.cid)
-        if (!cart){
-            return res.status(404).send({ERROR:`cart id ${req.cid} NOT FOUND.`})
+        if (!cart) {
+            return res.status(404).send({ ERROR: `cart id ${req.cid} NOT FOUND.` })
         }
         let product = await ProductManager.getProductById(req.pid)
-        if (!product){
-            return res.status(404).send({ERROR:`product id ${req.pid} NOT FOUND.`})
+        if (!product) {
+            return res.status(404).send({ ERROR: `product id ${req.pid} NOT FOUND.` })
         }
 
         await CartManager.addOrderToCart(req.cid, req.pid)
-        return res.status(200).send({CONFIRMATION:"Order Saved."})
-        
+        return res.status(200).send({ CONFIRMATION: "Order Saved." })
+
     } catch (err) {
         return res.status(500).send({ ERROR: `${err.message}` })
     }
+})
+
+router.delete("/:cid", cidCheck, async (req, res) => {
+    if (!isValidObjectId(req.cid)) {
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(400).json({ error: `Invalid Cart ID!` })
+    }
+    try {
+        let cart = await CartManager.getCartById(req.cid)
+        if (!cart) {
+            return res.status(404).send({ ERROR: `cart id ${req.cid} NOT FOUND.` })
+        }
+        if (cart.products.length == 0){
+            return res.status(404).send({ ERROR: `cart id ${req.cid} IS EMPTY.` })
+        }
+        await CartManager.emptyCart(req.cid)
+        return res.status(200).send({ CONFIRMATION: `Cart ${req.cid} is now empty.`, cart })
+
+    } catch (err) {
+        return res.status(500).send({ ERROR: `${err.message}` })
+    }
+})
+
+router.delete("/:cid/products/:pid", cidCheck, pidCheck, async (req, res) => {
+    if (!isValidObjectId(req.cid)) {
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(400).json({ error: `Invalid Cart ID!` })
+    }
+    if (!isValidObjectId(req.pid)) {
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(400).json({ error: `Invalid Product ID!` })
+    }
+    try {
+        let cart = await CartManager.getCartById(req.cid)
+        if (!cart) {
+            return res.status(404).send({ ERROR: `cart id ${req.cid} NOT FOUND.` })
+        }
+        let prodInCart = cart.products.find(prod => {
+            if (prod.product === req.pid) {
+                return prod
+            }
+        }
+        )
+        if (!prodInCart){
+            return res.status(404).send({ ERROR: `product of id ${req.pid} NOT FOUND in cart.` })
+        }
+        await CartManager.removeProduct(req.cid, req.pid)
+        return res.status(200).send({ CONFIRMATION: "Product Removed." })
+
+    } catch (error) {
+        return res.status(500).send({ ERROR: `${error.message}` })
+    }
+
+})
+
+router.put("/:cid/products/:pid", cidCheck, pidCheck, async (req, res) => {
+    if (!isValidObjectId(req.cid)) {
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(400).json({ error: `Invalid Cart ID!` })
+    }
+    if (!isValidObjectId(req.pid)) {
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(400).json({ error: `Invalid Product ID!` })
+    }
+    try {
+        let cart = await CartManager.getCartById(req.cid)
+        if (!cart) {
+            return res.status(404).send({ ERROR: `cart id ${req.cid} NOT FOUND.` })
+        }
+        let prodInCart = cart.products.find(prod => {
+            if (prod.product === req.pid) {
+                return prod
+            }
+        }
+        )
+        if (!prodInCart){
+            return res.status(404).send({ ERROR: `product of id ${req.pid} NOT FOUND in cart.` })
+        }
+
+        let {quantity} = req.body
+        quantity = Number(quantity)
+        await CartManager.updateCartProduct(req.cid, req.pid, quantity)
+        return res.status(200).send({ CONFIRMATION: `Product ${req.pid} quantity updated (now: ${quantity}).`})
+    } catch (error) {
+        return res.status(500).send({ ERROR: `${error.message}` })
+    }
+
 })
